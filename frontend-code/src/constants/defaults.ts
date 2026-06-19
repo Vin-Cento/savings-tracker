975aa2e2e89aeb4fa1a3dc0110fcf73c4217d603
Vincent Liu <vincentliu362@gmail.com>
2026-06-18 22:40:50 -0400

    feat: set the default item

--- frontend-code/src/constants/defaults.ts
+++ frontend-code/src/constants/defaults.ts
@@ -1,3 +1,7 @@
 import type { GoalSchema } from "../client/types.gen.ts";
 
-export const emptyGoal: GoalSchema = { id: -1, name: '', target: 0, active: true, deadline: "2026-06-18 13:00:59.978522-04", createdAt: "2026-06-18 13:00:59.978522-04" };
+export const emptyGoal: GoalSchema = {
+  id: -1, name: '', target: 0, active: true, deadline: new Date(
+    new Date().setMonth(new Date().getMonth() + 1)
+  ).toISOString(), createdAt: new Date().toISOString()
+};
