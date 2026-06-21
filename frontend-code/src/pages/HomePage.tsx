a44adfe66c4fe5176813c398ee4698a4ef4f809b
Vincent Liu <vincentliu362@gmail.com>
2026-06-21 10:55:20 -0400

    feat: add the status

--- frontend-code/src/pages/HomePage.tsx
+++ frontend-code/src/pages/HomePage.tsx
@@ -1,28 +1,28 @@
-// import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 import { formatMoney } from "../composables/format";
 
 function HomePage() {
   return (
     <>
       <div className="flex gap-x-2 m-2">
-        <div className="m-2 w-2/4 flex p-4 bg-amber-700 rounded-xl" >
+        <Link to={"/goals/management"} className="m-2 w-2/4 flex p-4  bg-linear-to-r from-red-700 to-amber-700 rounded-xl" >
           <div>
-            <h1>Total savings</h1>
-            <p className="text-5xl">{formatMoney(100000)}</p>
+            <h1 className="mb-5">Total savings</h1>
+            <p className="text-5xl font-bold">{formatMoney(100000)}</p>
           </div>
-        </div>
-        <div className="m-2 w-1/4 flex p-4 items-center bg-zinc-800 rounded-xl" >
+        </Link>
+        <Link to={"/goals/management"} className="m-2 w-1/4 flex p-4 items-center bg-zinc-800 rounded-xl" >
           <div>
-            <h1>Active goals</h1>
-            <p className="text-5xl">2</p>
+            <h1 className="mb-5">Active goals</h1>
+            <p className="text-5xl font-bold text-orange-600">2</p>
           </div>
-        </div>
-        <div className="m-2 w-1/4 flex p-4 items-center bg-zinc-800 rounded-xl" >
+        </Link>
+        <Link to={"/goals/management"} className="m-2 w-1/4 flex p-4 items-center bg-zinc-800 rounded-xl" >
           <div>
-            <h1>Goals completed</h1>
-            <p className="text-5xl">0</p>
+            <h1 className="mb-5">Goals completed</h1>
+            <p className="text-5xl font-bold text-green-400">0</p>
           </div>
-        </div>
+        </Link>
       </div>
     </>
   );
