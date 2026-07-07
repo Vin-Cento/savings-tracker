d0e82603871612f26f58256d0b472f4930ebcff2
Vincent Liu <vincentliu362@gmail.com>
2026-07-07 09:41:48 -0400

    feat: move add to navbar

--- frontend-code/src/components/NavBar.tsx
+++ frontend-code/src/components/NavBar.tsx
@@ -1,7 +1,13 @@
 import { Link } from "react-router-dom"
 import logoImage from '../assets/images/logo-large.svg'
+import { useDispatch } from "react-redux"
+import type { AppDispatch } from "../stores/store";
+import { setGoal } from "../stores/goalSlice";
+import { openAddGoalPopup } from "../stores/popupSlice";
+import { emptyGoal } from "../constants/defaults";
 
 function Navbar() {
+  const dispatch = useDispatch<AppDispatch>();
   return (
     <nav className="flex bg-zinc-900 h-13 items-center shrink-0">
       <li className="h-full flex items-center justify-center">
@@ -11,12 +17,13 @@ function Navbar() {
       </li>
       <div className="flex-1" />
       <button
-        className="pl-2 pr-2 p-1 m-2 bg-green-500 text-black rounded-xl font-bold"
+        className="pl-2 pr-2 p-1 m-2 bg-orange-500 text-black rounded-xl font-bold"
         onClick={() => {
-          console.log('create goal')
+          dispatch(setGoal({ goal: emptyGoal }))
+          dispatch(openAddGoalPopup())
         }}
       >
-        Create Goal
+        + New Goal
       </button>
     </nav>
   )
